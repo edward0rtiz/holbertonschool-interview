8b64b033224802db5851f84d084567530315d5e5
edward0rtiz <931@holbertonschool.com>
2020-04-27 00:06:18 -0500

    [fix] refactor code to use iteration over recusion due this problem exeeds the limit of operations

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -8,36 +8,23 @@ Task: Write a method that determines if all the boxes can be opened.
 """
 
 
-def search_box(boxes, unlocked_box, n_box):
-    """
-    Recursive function to iterate over the boxes and open if the n_box
-    is the same as the key_box
-    """
-    if len(n_box) > 0:
-        for box_key in n_box:
-            if (box_key < len(boxes)) and (box_key not in unlocked_box):
-                unlocked_box.insert(-1, box_key)
-                search_again = search_box(boxes, unlocked_box, boxes[box_key])
-                unlocked_box = search_again
-        return unlocked_box
-    else:
-        return unlocked_box
-
-
 def canUnlockAll(boxes):
     """
     Function that checks with boolean value if the list type and
-    length to invoke recursive function was True or not (False)
+    length to invoke two for iterations one to traverse the list
+    and the other to compaer if key is idx or not in order to open
     """
     if type(boxes) is not list:
         return False
     elif (len(boxes)) == 0:
         return False
-    for k_box in boxes:
-        if type(k_box) is not list:
-            return False
-    boxes_checked = search_box(boxes, [0], boxes[0])
-    if len(boxes_checked) == len(boxes):
-        return True
-    else:
-        return False
+    for k in range(1, len(boxes) - 1):
+        boxes_checked = False
+        for idx in range(len(boxes)):
+            boxes_checked = k in boxes[idx] and k != idx
+            if boxes_checked:
+                break
+        if boxes_checked is False:
+            return boxes_checked
+    return True
+
