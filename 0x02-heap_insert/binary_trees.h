0da0a1e15bdaf808f490528ba52f09d9bab4ed68
edward0rtiz <931@holbertonschool.com>
2020-05-05 21:24:45 -0500

    [feature] add libraries

---
+++ 0x02-heap_insert/binary_trees.h
@@ -0,0 +1,36 @@
+#ifndef _BINARY_TREES_H_
+#define _BINARY_TREES_H_
+
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * struct binary_tree_s - Binary tree node
+ *
+ * @n: Integer stored in the node
+ * @parent: Pointer to the parent node
+ * @left: Pointer to the left child node
+ * @right: Pointer to the right child node
+ */
+typedef struct binary_tree_s
+{
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
+} binary_tree_t;
+
+void binary_tree_print(const binary_tree_t *);
+
+/* Max binary heap */
+typedef struct binary_tree_s heap_t;
+
+/* create a new node prototype */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+
+/* insert value into max binary heap */
+heap_t *heap_insert(heap_t **root, int value);
+
+
+#endif /* _BINARY_TREES_H_ */
