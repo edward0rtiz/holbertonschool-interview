84edbbc18efa337f2169e5710de40a127d3a6245
edward0rtiz <931@holbertonschool.com>
2020-05-10 21:08:11 -0500

    [feature] minimum operations in python

---
+++ 0x03-minimum_operations/0-minoperations.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+""" Script that computes a minimum operations
+    needed in a CopyAll - Paste task
+"""
+
+
+def minOperations(n):
+    """
+    Method for compute the minimum number
+    of operations for task Copy All and Paste
+
+    Args:
+        n: input value
+        factor_list: List to save the operations
+    Return: the sum of the operations
+    """
+    if n < 2:
+        return 0
+    factor_list = []
+    i = 1
+    while n != 1:
+        i += 1
+        if n % i == 0:
+            while n % i == 0:
+                n /= i
+                factor_list.append(i)
+    return sum(factor_list)
