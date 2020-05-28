3d62c8cbc8dba852818ed8496290ea07773b263d
edward0rtiz <931@holbertonschool.com>
2020-05-27 21:31:20 -0500

    [update] recursion fucntion gp

--- 0x05-linked_list_palindrome/lists.h
+++ 0x05-linked_list_palindrome/lists.h
@@ -23,5 +23,6 @@ listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
 
 int is_palindrome(listint_t **head);
+int recursive_palindrome(listint_t *head, listint_t *tail);
 
 #endif /* LISTS_H */
