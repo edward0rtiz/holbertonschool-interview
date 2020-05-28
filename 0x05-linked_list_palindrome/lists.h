3e04f960990989cf689b289168824261b97858ad
edward0rtiz <931@holbertonschool.com>
2020-05-27 21:42:11 -0500

    [fix] betty at lists

--- 0x05-linked_list_palindrome/lists.h
+++ 0x05-linked_list_palindrome/lists.h
@@ -14,8 +14,8 @@
  */
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 
 size_t print_listint(const listint_t *h);
@@ -23,6 +23,6 @@ listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
 
 int is_palindrome(listint_t **head);
-int recursive_palindrome(listint_t *head, listint_t *tail);
+int recursive_palindrome(listint_t **head, listint_t *tail);
 
 #endif /* LISTS_H */
