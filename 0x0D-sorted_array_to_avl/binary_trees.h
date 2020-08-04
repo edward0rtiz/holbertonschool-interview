9720bd28a4eef8daa3bd3c375455ef83a399f60a
edward0rtiz <931@holbertonschool.com>
2020-08-03 22:25:10 -0500

    [feature] binary trees.h

---
+++ 0x0D-sorted_array_to_avl/binary_trees.h
@@ -0,0 +1,27 @@
+#ifndef _BINARY_TREES_H_
+#define _BINARY_TREES_H_
+
+#include <stddef.h>
+
+/**
+ * struct binary_tree_s - Binary tree node
+ *
+ * @n: Integer stored in the node
+ * @parent: Pointer to the parent node
+ * @left: Pointer to the left child node
+ * @right: Pointer to the right child node
+ */
+struct binary_tree_s
+{
+    int n;
+    struct binary_tree_s *parent;
+    struct binary_tree_s *left;
+    struct binary_tree_s *right;
+};
+
+typedef struct binary_tree_s binary_tree_t;
+void binary_tree_print(const binary_tree_t *);
+typedef struct binary_tree_s avl_t;
+avl_t *sorted_array_to_avl(int *array, size_t size);
+
+#endif /* _BINARY_TREES_H_ */
