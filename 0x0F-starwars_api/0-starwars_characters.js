f854cc536a64859a4ef625d7ce1473652a650df3
edward0rtiz <931@holbertonschool.com>
2020-08-19 20:29:00 -0500

    [feature] starwars using request

---
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+
+const request = require('request');
+const filmId = process.argv[2];
+const url = 'https://swapi-api.hbtn.io/api/films/$(filmId)/';
+request(url, async function (error, response, body) {
+  if (error) {
+    return console.log(error);
+  } else {
+    const characters = JSON.parse(body).characters;
+    for (const character in characters) {
+	    const res = await new Promise((resolve, reject) => {
+        request(characters[character], (err, res, html) => {
+		    if (err) { reject(err); } else {
+            resolve(JSON.parse(html).name);
+		    }
+        });
+	    });
+	    console.log(res);
+    }
+  }
+});
