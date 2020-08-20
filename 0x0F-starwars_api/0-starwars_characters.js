08433a76d9506e2af8a9dbacaf3dda5bfeca3ae9
edward0rtiz <931@holbertonschool.com>
2020-08-19 21:02:46 -0500

    [fix] ln 7 declare var of ur filmId using curly bracket and fix semistandard

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -1,22 +1,23 @@
 #!/usr/bin/node
 
 const request = require('request');
-const filmId = process.argv[2];
-const url = 'https://swapi-api.hbtn.io/api/films/$(filmId)/';
+const url = `https://swapi-api.hbtn.io/api/films/${process.argv[2]}/`;
 request(url, async function (error, response, body) {
   if (error) {
     return console.log(error);
   } else {
     const characters = JSON.parse(body).characters;
     for (const character in characters) {
-	    const res = await new Promise((resolve, reject) => {
+      const res = await new Promise((resolve, reject) => {
         request(characters[character], (err, res, html) => {
-		    if (err) { reject(err); } else {
+          if (err) {
+            reject(err);
+          } else {
             resolve(JSON.parse(html).name);
-		    }
+          }
         });
-	    });
-	    console.log(res);
+      });
+      console.log(res);
     }
   }
 });
