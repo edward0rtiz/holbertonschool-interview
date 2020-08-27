345d0c1312024dd046d6994dc42fa35c8b3f2328
edward0rtiz <931@holbertonschool.com>
2020-08-26 21:58:03 -0500

    [feature] rain test

---
+++ 0x10-rain/0-rain.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""Trapping rain water"""
+
+
+def water(arr, n):
+    max_size = 0
+
+    for i in range(1, n - 1):
+        left = arr[i]
+        for j in range(i):
+            left = max(left, arr[j])
+            right = arr[i]
+        for j in range(i + 1, n):
+            right = max(right, arr[j])
+
+        max_size = max_size + (min(left, right) - arr[i])
+
+    return max_size
+
+
+def rain(walls):
+    water_saved = water(walls, len(walls))
+    return water_saved
