75018134e1a0f513fa7be595a5f410c8f342b821
edward0rtiz <931@holbertonschool.com>
2020-10-08 11:26:00 -0500

    [feaute] upload menger code

---
+++ 0x0B-menger/0-menger.c
@@ -0,0 +1,39 @@
+#include "menger.h"
+
+/**
+ * menger - 2D menger sponge
+ * @level: level of the menger sponge
+ */
+
+void menger(int level)
+{
+	int size, row, col;
+	char character;
+
+	size = pow(3, level);
+
+	for (row = 0; row < size; row++)
+	{
+		for (col = 0; col < size; col++)
+		{
+			character = get_character(row, col);
+			printf("%c", character);
+		}
+		printf("\n");
+	}
+}
+/**
+ * get_character - get the character
+ * @level: level of the menger sponge
+ */
+char get_character(int row, int col)
+{
+	while (row || col)
+	{
+		if (row % 3 == 1 && col % 3 == 1)
+			return (' ');
+		row /= 3;
+		col /= 3;
+	}
+	return ('#');
+}
---
+++ 0x0B-menger/menger.h
@@ -0,0 +1,10 @@
+#ifndef MENGER_H
+#define MENGER_H
+
+#include <stdio.h>
+#include <math.h>
+
+void menger(int level);
+char get_character(int row, int col);
+
+#endif /*MENGER_H*/
