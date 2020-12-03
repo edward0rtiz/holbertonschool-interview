ab0301339564b680b99f3046c0541a651698aa6b
edward0rtiz <931@holbertonschool.com>
2020-12-02 20:39:29 -0500

    [feature] adding library file

---
+++ 0x1B-radix_sort/sort.h
@@ -0,0 +1,13 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+
+#include <stddef.h>
+#include <stdlib.h>
+
+int _putchar(char);
+
+void print_array(const int *array, size_t size);
+
+void radix_sort(int *array, size_t size);
+
+#endif
