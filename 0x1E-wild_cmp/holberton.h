1ce6936c09cefd72d292e4ebd2acc723b502360c
edward0rtiz <931@holbertonschool.com>
2020-12-19 22:14:43 -0500

    [fix] holberton.h

--- 0x1E-wild_cmp/holberton.h
+++ 0x1E-wild_cmp/holberton.h
@@ -1,6 +1,10 @@
-#ifndef H_WILD_CMP
-#define H_WILD_CMP
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+/* function */
 int wildcmp(char *s1, char *s2);
 
-#endif
+#endif /* _HOLBERTON_H_ */
